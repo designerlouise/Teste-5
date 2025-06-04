ff707d8d3652e660a8322f977346f45ee7cf3d58
designerlouise <designlouisegrafico@gmail.com>
2025-06-04 19:11:48 -0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -42,7 +42,6 @@ function Footer() {
               <h3 className="text-white font-bold mb-4">Contato</h3>
               <div className="space-y-2 text-gray-400">
                 <p><i className="fas fa-envelope mr-2 netflix-red-text"></i> designlouisegrafico@gmail.com</p>
-                <p><i className="fas fa-phone mr-2 netflix-red-text"></i> (32) 99110-2463</p>
                 <p><i className="fab fa-whatsapp mr-2 netflix-red-text"></i> (32) 99110-2463</p>
                 <p><i className="fas fa-map-marker-alt mr-2 netflix-red-text"></i> Rodeiro, MG</p>
               </div>
@@ -50,7 +49,7 @@ function Footer() {
           </div>
 
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-500">
-            <p>&copy; 2025 Louiseflix _ Designer Louise. Todos os direitos reservados.</p>
+            <p>&copy; 2025 developed by Designer Louise. Todos os direitos reservados.</p>
           </div>
         </div>
       </footer>
